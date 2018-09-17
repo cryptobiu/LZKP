dba53b0a9e3825c5e785562f69c3ac61b9692f97
Roee Sefi <roee.sefi@gmail.com>
2018-09-17 08:22:09 +0300

    Automation scripts - update parser

--- scripts/parseMeasurements.py
+++ scripts/parseMeasurements.py
@@ -33,6 +33,9 @@ if len(sys.argv) != 4:
 	print 'usage: ./parseMeasurements.py <execServers-file> <measurments-file> <results-file>'
 	sys.exit(1)
 
+book = xlwt.Workbook()
+sheet = book.add_sheet("v1.5", cell_overwrite_ok=True)
+
 config = open(sys.argv[1], 'r').readlines()
 config = [c.split('#')[0] for c in config]
 q = [c.replace('\n', '').replace('\t', '').strip() for c in config if c.startswith('q=(')]
@@ -61,9 +64,24 @@ print 'Execution time: {}'.format(measurements[1])
 m_id = 2
 r_id = 2
 
+row_id = 0
+col_id = 3
+for c, v in enumerate(['q', 'n', 'm', 'N', 'M', 'tau', 'MT?', 'total times', 'AVG', 'STD', 'computation times', 'AVG', 'STD']):
+	if v == 'total times' or v == 'computation times':
+		sheet.write_merge(row_id, row_id, col_id, col_id + num_trials - 1, v)
+		col_id += num_trials
+	else:
+		sheet.write(row_id, col_id, v)	
+		col_id += 1
+row_id += 1
+
+
 print '\t\t{:<5}{:<5}{:<7}{:<5}{:<5}{:<5}{:<5}{:<75}{:<12}{:<12}{:<75}{:<12}{:<12}'.format('q', 'n', 'm', 'N', 'M', 'tau', 'MT?', 'total times', 'AVG', 'STD', 'computation times', 'AVG', 'STD')
 for i_protocol, protocol in enumerate(['1', '2']):
 	print measurements[m_id]
+	sheet.write(row_id, 0, measurements[m_id])
+	row_id += 1
+
 	assert measurements[m_id] == 'protocol {}'.format(protocol)
 	m_id += 1
 
@@ -93,6 +111,19 @@ for i_protocol, protocol in enumerate(['1', '2']):
 
 				m_id += 1
 
+				col_id = 3
+				for c, (v1, v2) in enumerate(zip(['q', 'n', 'm', 'N', 'M', 'tau', 'MT?', 'total times', 'AVG', 'STD', 'computation times', 'AVG', 'STD'],
+										         [qq, nn, mm, NN, MMM, TTT, '', ','.join(map(str, mmm)), mean(mmm), stddev(mmm), ','.join(map(str, ccc)), mean(ccc), stddev(ccc)])):
+					if v1 == 'total times' or v1 == 'computation times':
+						for vv2 in v2.split(','):
+							sheet.write(row_id, col_id, vv2)
+							col_id += 1
+					else:
+						sheet.write(row_id, col_id, v2)
+						col_id += 1
+
+				row_id += 1
+
 	qq = q[i_protocol].split('=')[1][1:-1].replace('"', '').split()[-1]
 	nn = n[i_protocol].split('=')[1][1:-1].replace('"', '').split()[-1]
 	mm = m[i_protocol].split('=')[1][1:-1].replace('"', '').split()[-1]
@@ -120,4 +151,17 @@ for i_protocol, protocol in enumerate(['1', '2']):
 
 			m_id += 1
 
-			#print qq, nn, mm, NN, MMM, TTT, XX
+			col_id = 3
+			for c, (v1, v2) in enumerate(zip(['q', 'n', 'm', 'N', 'M', 'tau', 'MT?', 'total times', 'AVG', 'STD', 'computation times', 'AVG', 'STD'],
+										     [qq, nn, mm, NN, MMM, TTT, XXX, ','.join(map(str, mmm)), mean(mmm), stddev(mmm), ','.join(map(str, ccc)), mean(ccc), stddev(ccc)])):
+				if v1 == 'total times' or v1 == 'computation times':
+					for vv2 in v2.split(','):
+						sheet.write(row_id, col_id, vv2)
+						col_id += 1
+				else:
+					sheet.write(row_id, col_id, v2)
+					col_id += 1
+
+			row_id += 1
+
+book.save('stats.xls')
