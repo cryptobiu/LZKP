987862450d6ceb88a47e9f51dcdb43261f229a78
Roee Sefi <roee.sefi@gmail.com>
2018-09-18 00:19:36 +0300

    Update automation scripts for v2.0

---
+++ scripts/execClients_v2.0.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/bash
+
+PORT=8000
+IP=127.0.0.1
+
+NUM_TRIALS=10
+
+git log -1 | head -1 > results.txt
+date +"%Y/%m/%d %H:%M:%S.%N" >> results.txt
+
+# Best to copy parameters section from execServers config file
+q=("15" "15" "31" "59" "61")
+n=("256" "256" "512" "1024" "1024")
+m=("1024" "4096" "2048" "4096" "4096")
+
+N=("2" "4" "8" "16" "32" "64")
+M=("75:145" "55:105" "55:95" "45:95" "45:85" "45:85")
+tau=("34:63" "32:57" "38:57" "26:63" "28:47" "28:49")
+for((p=0;p<${#q[@]};p++)); do
+    for((i=0;i<${#N[@]};i++)); do
+        MM=(${M[$i]//:/ })
+        TT=(${tau[$i]//:/ })
+
+        for((j=0;j<${#MM[@]};j++)); do
+        	for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        		echo "../LZKP -p0 -q${q[$p]} --ip ${IP} --port ${PORT}"
+        		S=$(../LZKP -p0 -q${q[$p]} --ip ${IP} --port ${PORT}) # Single threaded
+        		echo ${S} | tee -a results.txt
+        		wait
+        		sleep 1
+        	done
+
+        	# for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        		# echo "../LZKP -p0 -q${q[$p]} --ip ${IP} --port ${PORT} -x8"
+        		# S=$(echo "../LZKP -p0 -q${q[$p]} --ip ${IP} --port ${PORT} -x8") # Single threaded
+        		# echo ${S} | tee -a results.txt
+        		# wait
+        		# sleep 1
+        	# done
+        done
+    done
+done
+
+X=("2" "4" "8" "16" "32")
+for((x=0;x<${#X[@]};x++)); do
+    R=""
+    for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        echo "../LZKP -p0 -q61 --ip ${IP} --port ${PORT} -x${X[$x]}"
+        S=$(../LZKP -p0 -q61 --ip ${IP} --port ${PORT} -x${X[$x]}) # Multi threaded
+        echo ${S} | tee -a results.txt
+        wait
+        sleep 1
+    done
+
+    R=""
+    for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        echo "../LZKP -p0 -q61 --ip ${IP} --port ${PORT} -x${X[$x]}"
+        S=$(../LZKP -p0 -q61 --ip ${IP} --port ${PORT} -x${X[$x]}) # Multi threaded
+        echo ${S} | tee -a results.txt
+        wait
+        sleep 1
+    done
+done
+
+
+q=("15" "15" "31" "59" "61")
+n=("256" "256" "512" "1024" "1024")
+m=("1024" "4096" "2048" "4096" "4096")
+
+N=("2" "4" "8" "16" "32" "64")
+M=("40:80" "20:40" "14:27" "10:20" "8:16" "7:14")
+for((p=0;p<${#q[@]};p++)); do
+    for((i=0;i<${#N[@]};i++)); do
+        MM=(${M[$i]//:/ })
+
+        for((j=0;j<${#MM[@]};j++)); do
+        	for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        		echo "../LZKP -p1 -q${q[$p]} --ip ${IP} --port ${PORT}"
+        		S=$(../LZKP -p1 -q${q[$p]} --ip ${IP} --port ${PORT}) # Single threaded
+        		echo ${S} | tee -a results.txt
+        		wait
+        		sleep 1
+        	done
+
+        	# for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        		# echo "../LZKP -p1 -q${q[$p]} --ip ${IP} --port ${PORT} -x8"
+        		# S=$(echo "../LZKP -p1 -q${q[$p]} --ip ${IP} --port ${PORT} -x8") # Multi threaded
+        		# echo ${S} | tee -a results.txt
+        		# wait
+        		# sleep 1
+        	# done
+        done
+    done
+done
+
+X=("2" "4" "8" "16" "32")
+for((x=0;x<${#X[@]};x++)); do
+    R=""
+    for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        echo "../LZKP -p1 -q61 --ip ${IP} --port ${PORT} -x${X[$x]}"
+        S=$(../LZKP -p1 -q61 --ip ${IP} --port ${PORT} -x${X[$x]}) # Multi threaded
+        echo ${S} | tee -a results.txt
+        wait
+        sleep 1
+    done
+
+    R=""
+    for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        echo "../LZKP -p1 -q61 --ip ${IP} --port ${PORT} -x${X[$x]}"
+        S=$(../LZKP -p1 -q61 --ip ${IP} --port ${PORT} -x${X[$x]}) # Multi threaded
+        echo ${S} | tee -a results.txt
+        wait
+        sleep 1
+    done
+done
+
+date +"%Y/%m/%d %H:%M:%S.%N" >> results.txt
---
+++ scripts/execServers_v2.0.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/bash
+
+PORT=8000
+
+NUM_TRIALS=10
+
+git log -1 | head -1 > measurements.txt
+date +"%Y/%m/%d %H:%M:%S.%N" | tee -a measurements.txt
+
+
+# Parameters are going in parallel, first entry of each parameter are corresponding
+q=("15" "15" "31" "59" "61")
+n=("256" "256" "512" "1024" "1024")
+m=("1024" "4096" "2048" "4096" "4096")
+
+N=("2" "4" "8" "16" "32" "64")
+M=("75:145" "55:105" "55:95" "45:95" "45:85" "45:85")
+tau=("34:63" "32:57" "38:57" "26:63" "28:47" "28:49")
+echo "protocol 1" | tee -a measurements.txt
+for((p=0;p<${#q[@]};p++)); do
+    for((i=0;i<${#N[@]};i++)); do
+        MM=(${M[$i]//:/ })
+        TT=(${tau[$i]//:/ })
+
+        for((j=0;j<${#MM[@]};j++)); do
+        	R=""
+        	for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        		echo "../LZKP -p0 -i -q${q[$p]} -M${MM[$j]} -t${TT[$j]} -N${N[$i]} -n${n[$p]} -m${m[$p]} -a --port ${PORT}"
+        		S=$(../LZKP -p0 -i -q${q[$p]} -M${MM[$j]} -t${TT[$j]} -N${N[$i]} -n${n[$p]} -m${m[$p]} -a --port ${PORT}) # Single threaded
+        		R="${R}${S} "
+        		wait
+        	done
+        	echo ${R} | tee -a measurements.txt
+
+        	# R=""
+        	# for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        		# echo "../LZKP -p0 -i -q${q[$p]} -M${MM[$j]} -t${TT[$j]} -N${N[$i]} -n${n[$p]} -m${m[$p]} -a --port ${PORT} -x8"
+        		#S=$(echo "../LZKP -p0 -i -q${q[$p]} -M${MM[$j]} -t${TT[$j]} -N${N[$i]} -n${n[$p]} -m${m[$p]} -a --port ${PORT} -x8") # Multi threaded
+        		#R="${R}${S} "
+        		# wait
+        	# done
+        	# echo ${R} >> measurements.txt
+        done
+    done
+done
+
+X=("2" "4" "8" "16" "32")
+for((x=0;x<${#X[@]};x++)); do
+    R=""
+    for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        echo "../LZKP -p0 -i -q61 -M45 -t28 -N64 -n1024 -m4096 -a --port ${PORT} -x${X[$x]}" # Multi threaded
+        S=$(../LZKP -p0 -i -q61 -M45 -t28 -N64 -n1024 -m4096 -a --port ${PORT} -x${X[$x]})
+        R="${R}${S} "
+        wait
+    done
+    echo ${R} | tee -a measurements.txt
+
+    R=""
+    for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        echo "../LZKP -p0 -i -q61 -M85 -t49 -N64 -n1024 -m4096 -a --port ${PORT} -x${X[$x]}" # Multi threaded
+        S=$(../LZKP -p0 -i -q61 -M85 -t49 -N64 -n1024 -m4096 -a --port ${PORT} -x${X[$x]})
+        R="${R}${S} "
+        wait
+    done
+    echo ${R} | tee -a measurements.txt
+done
+
+
+q=("15" "15" "31" "59" "61")
+n=("256" "256" "512" "1024" "1024")
+m=("1024" "4096" "2048" "4096" "4096")
+
+N=("2" "4" "8" "16" "32" "64")
+M=("40:80" "20:40" "14:27" "10:20" "8:16" "7:14")
+echo "protocol 2" | tee -a measurements.txt
+for((p=0;p<${#q[@]};p++)); do
+    for((i=0;i<${#N[@]};i++)); do
+        MM=(${M[$i]//:/ })
+
+        for((j=0;j<${#MM[@]};j++)); do
+        	R=""
+        	for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        		echo "../LZKP -p1 -i -q${q[$p]} -M${MM[$j]} -t1 -N${N[$i]} -n${n[$p]} -m${m[$p]} -a --port ${PORT}"
+        		S=$(../LZKP -p1 -i -q${q[$p]} -M${MM[$j]} -t1 -N${N[$i]} -n${n[$p]} -m${m[$p]} -a --port ${PORT}) # Single threaded
+        		R="${R}${S} "
+        		wait
+        	done
+        	echo ${R} | tee -a measurements.txt
+
+        	# R=""
+        	# for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        		# echo "../LZKP -p1 -i -q${q[$p]} -M${MM[$j]} -t1 -N${N[$i]} -n${n[$p]} -m${m[$p]} -a --port ${PORT} -x8"
+        		#S=$(echo "../LZKP -p1 -i -q${q[$p]} -M${MM[$j]} -t1 -N${N[$i]} -n${n[$p]} -m${m[$p]} -a --port ${PORT} -x8") # Multi threaded
+        		#R="${R}${S} "
+        		# wait
+        	# done
+        	# echo ${R} | tee -a measurements.txt
+        done
+    done
+done
+
+X=("2" "4" "8" "16" "32")
+for((x=0;x<${#X[@]};x++)); do
+    R=""
+    for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        echo "../LZKP -p1 -i -q61 -M7 -t1 -N64 -n1024 -m4096 -a --port ${PORT} -x${X[$x]}" # Multi threaded
+        S=$(../LZKP -p1 -i -q61 -M7 -t1 -N64 -n1024 -m4096 -a --port ${PORT} -x${X[$x]})
+        R="${R}${S} "
+        wait
+    done
+    echo ${R} | tee -a measurements.txt
+
+    R=""
+    for((nt=0;nt<${NUM_TRIALS};nt++)); do
+        echo "../LZKP -p1 -i -q61 -M14 -t1 -N64 -n1024 -m4096 -a --port ${PORT} -x${X[$x]}" # Multi threaded
+        S=$(../LZKP -p1 -i -q61 -M14 -t1 -N64 -n1024 -m4096 -a --port ${PORT} -x${X[$x]})
+        R="${R}${S} "
+        wait
+    done
+    echo ${R} | tee -a measurements.txt
+done
+
+date +"%Y/%m/%d %H:%M:%S.%N" | tee -a measurements.txt
