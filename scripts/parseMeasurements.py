b5799423aed0971166c3eda492cedcc5f887f3c3
Roee Sefi <roee.sefi@gmail.com>
2018-10-02 20:56:30 +0300

    Add results for v3.0

--- scripts/parseMeasurements.py
+++ scripts/parseMeasurements.py
@@ -153,14 +153,14 @@ for i_protocol, protocol in enumerate(['1', '2']):
 
 				row_id += 1
 
-	qq = q[i_protocol].split('=')[1][1:-1].replace('"', '').split()[-1]
-	nn = n[i_protocol].split('=')[1][1:-1].replace('"', '').split()[-1]
-	mm = m[i_protocol].split('=')[1][1:-1].replace('"', '').split()[-1]
-	NN = N[i_protocol].split('=')[1][1:-1].replace('"', '').split()[-1]
-	MM = M[i_protocol].split('=')[1][1:-1].replace('"', '').split()[-1]
+	qq = q[i_protocol].split('=')[1][1:-1].replace('"', '').split()[0]
+	nn = n[i_protocol].split('=')[1][1:-1].replace('"', '').split()[0]
+	mm = m[i_protocol].split('=')[1][1:-1].replace('"', '').split()[0]
+	NN = N[i_protocol].split('=')[1][1:-1].replace('"', '').split()[0]
+	MM = M[i_protocol].split('=')[1][1:-1].replace('"', '').split()[0]
 
 	try:
-		TT = tau[i_protocol].split('=')[1][1:-1].replace('"', '').split()[-1]
+		TT = tau[i_protocol].split('=')[1][1:-1].replace('"', '').split()[0]
 	except:
 		TT = ':'.join(['' for _ in range(len(MM.split(':')))])
 
